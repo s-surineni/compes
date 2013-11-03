6c02ddbe503072416d639992d994ad51ba43b43f
vinnu <sampath kumar>
2013-11-03 09:26:35 +0530

    arraylist and amp

--- src/compes/Maxcount.java
+++ src/compes/Maxcount.java
@@ -6,10 +6,33 @@
  */
 package compes;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 /**
  *
  * @author vinnu
  */
 public class Maxcount {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int testCases=Integer.parseInt(br.readLine());
+        int testNumbers[]=new int[testCases];
+        int testResults[]=new int[testCases];
+        while(testCases>0){
+            int noOfElements=Integer.parseInt(br.readLine());
+            ArrayList<Integer> elements=new ArrayList<Integer>();
+            Map<Integer,Integer> results=new HashMap<Integer,Integer>();
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            for(int i=0;i<noOfElements;i++){
+                elements.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+    }
     
 }
