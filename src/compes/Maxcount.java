400ebee829350f9631eb36dc506e63b250673047
vinnu <sampath kumar>
2013-11-04 20:50:58 +0530

    completed

--- src/compes/Maxcount.java
+++ src/compes/Maxcount.java
@@ -10,6 +10,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -32,6 +33,27 @@ public class Maxcount {
             for(int i=0;i<noOfElements;i++){
                 elements.add(Integer.parseInt(st.nextToken()));
             }
+            for(int j=0;j<noOfElements;j++){
+                results.put(elements.get(j), Collections.frequency(elements, elements.get(j)));
+            }
+            int number=0;
+            int maxValue=-1;
+            for(Integer value:results.keySet()){
+                if(results.get(value)>maxValue){
+                    maxValue=results.get(value);
+                    number=value;
+                }
+                else if(results.get(value)==maxValue){
+                    if(value<number)
+                        number=value;
+                }
+            }
+            testNumbers[testCases-1]=number;
+            testResults[testCases-1]=maxValue;
+            testCases--;
+        }
+        for(int k=testNumbers.length-1;k>=0;k--){
+            System.out.println(testNumbers[k]+" "+testResults[k]);
         }
     }
     
